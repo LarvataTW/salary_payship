6467515c3c7ec6ce8d50884075255bf2acaad396
smallsmall <liyushiuan@gmail.com>
2014-06-09 08:18:22 +0800

    Modified the part of payship list so that
    * User can delete payship record
    * The year and month are selected according to the user specification.

--- payship_list.php
+++ payship_list.php
@@ -3,7 +3,7 @@ require_once('hook.php');
 require_once('model/salary_payship.php');
 
 define("MIN_SEL_YEAR", 2013);
-define("MAX_SEL_YEAR", 2100);
+define("MAX_SEL_YEAR", 2030);
 define("MIN_SEL_MONTH", 1);
 define("MAX_SEL_MONTH", 12);
 
@@ -30,7 +30,7 @@ function regulate_month_input() {
     $result = array();
     if (!isset($_POST['month']) or !is_numeric($_POST['month'])) {
         $result['status'] = 'unspecified';
-        $result['month'] = date('m');
+        $result['month'] = sprintf("%02d", date('m'));
     } elseif ($_POST['month'] < MIN_SEL_MONTH) {
         $result['status'] = 'min';
         $result['month'] = MIN_SEL_MONTH;
@@ -46,75 +46,81 @@ function regulate_month_input() {
 
 }
 
-function set_list_content($dbcon) {
-    if (isset($_POST['year']) and isset($_POST['month'])) {
-        if (!is_numeric($_POST['year']) or !is_numeric($_POST['month'])) {
-            die("Please check the submitted data.");
-        }
+function set_list_content($dbcon, $year_info, $month_info) {
+
+    // message processing, to add later
+
+    $payship_date = $year_info['year'] . '-' . $month_info['month'] . '-' . '01';
+    echo "payship_date=$payship_date" . '<br/>';
+    $payship_array = summarize_payship($dbcon, $payship_date); 
+
+    if (empty($payship_array)) {
+        $html_list_content = "No payship is created for the specified month.";
+        return $html_list_content;
+    }
     
-        $month = sprintf("%02d", $_POST['month']);
-        $payship_date = $_POST['year'] . '-' . $month . '-' . '01';
-        $payship_array = summarize_payship($dbcon, $payship_date);   
+    //render payship_rows
+    $payship_rows   = '';
 
-        if (empty($payship_array)) {
-            $html_list_content = "No payship is created for the specified month.";
-            return $html_list_content;
-        }
-        
-        //render payship_rows
-        $payship_rows   = '';
-
-        foreach ($payship_array as $item) {
+    foreach ($payship_array as $item) {
 $table_row = <<<EOD
-        <tr>
-            <td>{$item['employee_id']}</td>
-            <td>{$item['name']}</td>
-            <td>{$item['basic_salary']}</td>
-            <td>{$item['finalwage']}</td>
-            <td><a href="edit_monthly_salary.php?action=edit">Edit</a> <a href="monthly_salary_process.php?action=delete&id={$item['payship_id']}">Delete</a></td>
-        </tr>
+    <tr>
+        <td>{$item['employee_id']}</td>
+        <td>{$item['name']}</td>
+        <td>{$item['basic_salary']}</td>
+        <td>{$item['finalwage']}</td>
+        <td><a href="edit_monthly_salary.php?action=edit">Edit</a> <a href="monthly_salary_process.php?action=delete&id={$item['payship_id']}">Delete</a></td>
+    </tr>
 EOD;
-        $payship_rows .= $table_row;
+    $payship_rows .= $table_row;
 
-        }
+    }
 
 
 $html_list_content = <<<EOD
-        <table>
-            <tr>
-                <td>員工編號</td>
-                <td>員工姓名</td>
-                <td>基本薪資</td>
-                <td>實領薪資</td>
-                <td>操作</td>
-            </tr>
-            {$payship_rows}
-        </table>
+    <table>
+        <tr>
+            <td>員工編號</td>
+            <td>員工姓名</td>
+            <td>基本薪資</td>
+            <td>實領薪資</td>
+            <td>操作</td>
+        </tr>
+        {$payship_rows}
+    </table>
 EOD;
-    }
-    else {
-        //die("Please check the submitted data.");
-        $html_list_content = "No month is specified yet.";
-    }
+
     return $html_list_content;
-}
- 
-if (isset($_POST['year']) and isset($_POST['year'])) {
-    $selected_year = $_POST['year'];
-}
-else {
+
 }
 
+ 
+$year_info = regulate_year_input();
+$month_info = regulate_month_input();
+
+print_r($year_info);
+echo '<br/>';
+print_r($month_info);
 
 $html_year_select = '<select name="year">';
-for ($year = 2013; $year < 2020; $year++) {
-    $html_year_select = $html_year_select . '<option value="' . $year . '">' . $year . '</option>';
+for ($year = MIN_SEL_YEAR; $year <= MAX_SEL_YEAR; $year++) {
+    if ($year != $year_info['year']) {
+        $html_year_select .= '<option value="' . $year . '">' . $year . '</option>';
+    } else {
+        $html_year_select .= '<option value="' . $year . '" selected>' . $year . '</option>';
+    }
+
 }
-$html_year_select = $html_year_select . '</select>';
+$html_year_select .= '</select>';
 
 $html_month_select = '<select name="month">';
-for ($month = 1; $month <= 12; $month++) {
-    $html_month_select = $html_month_select . '<option value="' . $month . '">' . $month . '</option>';
+for ($month = MIN_SEL_MONTH; $month <= MAX_SEL_MONTH; $month++) {
+    $month_value = sprintf("%02d", $month);
+    if ($month_value != $month_info['month']) {
+        $html_month_select .= '<option value="' . $month_value . '">' . $month_value . '</option>';
+    } else {
+        $html_month_select .= '<option value="' . $month_value . '" selected>' . $month_value . '</option>';
+    }
 }
 $html_month_select = $html_month_select . '</select>';
 
@@ -127,8 +133,7 @@ $html_form = <<<EOD
 </div>
 EOD;
 
-$html_list_content = set_list_content($dbcon);
-
+$html_list_content = set_list_content($dbcon, $year_info, $month_info);
 
 include 'layout_header';
 echo $html_form;
